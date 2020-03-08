594b8248f50fd71adeaadab6ae84d795dc85aa6f
HipolitoRosado <paulserrano58@gmail.com>
2020-03-08 11:58:15 -0500

    Add project files.

---
+++ Lesson7/GetSetExample.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson7
+{
+    class GetSetExample
+    {
+        private int _isProtected;
+        public int isProtected
+        {
+            get
+            { return _isProtected; }
+            set
+            {
+                if (value > 0)
+                {
+                    _isProtected = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Value must be greater than zero.  These are the rules!");
+                }
+            }
+        }
+        public string readOnly { get; }
+        public const string alwaysTheSame = "Never Changing Value";
+
+
+        private string _objName;
+        public string objName
+        {
+
+            get { return _objName; }
+            set
+            {
+                if (objName.Length > 3)
+                {
+                    _objName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("ObjName must be greater than 3 letters.");
+                }
+            }
+        }
+
+        public string goodEncapsulation { get; private set; }
+
+        public GetSetExample(string readOnly, string objName, int isProtected)
+        {
+            this.readOnly = readOnly;
+            this._objName = objName;
+            this._isProtected = isProtected;
+        }
+        public void ShowMeInternalName()
+        {
+            Console.WriteLine(_objName);
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Lesson7/WithEncap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson7
+{
+    class WithEncap
+    {
+        public int notProtected;
+        private int isProtected;
+
+        public WithEncap()
+        {
+            notProtected = 1;
+            isProtected = 1;
+        }
+
+        public void UpdatePrivateProperty(int newValue)
+        {
+            if (newValue < 0)
+            {
+                isProtected = newValue;
+            }
+        }
+    }
+}
